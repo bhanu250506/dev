e5a23a61046088144515e8b9b385b724792ee6e6
bhanu250506 <bhanupratap2556@gmail.com>
2025-09-03 22:21:09 +0530

    Update deploy config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-    base: '/new_website/'
+    base: '/new_website/',
+      build: {
+    chunkSizeWarningLimit: 1000, // increase from 500 KB → 1 MB
+  }
+
 })
